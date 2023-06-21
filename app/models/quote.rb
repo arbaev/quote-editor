baa81a2408efa2ebcdfc0dde700d42e30f39f34c
Tim Arbaev <timbrk@gmail.com>
2023-06-21 13:33:45 +0300

    feat: Quotes TurboStream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+# model for Quote
 class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  broadcasts_to ->(_quote) { "quotes" }, inserts_by: :prepend
 end
